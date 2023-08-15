d818d89874dae4b696dad9b471691e8f4f720f78
NatanDuarte <natanprocopio@gmail.com>
2023-08-15 12:39:33 -0300

    testing color extraction endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 
 from fastapi.testclient import TestClient
@@ -20,3 +21,18 @@ def test_on_verify_integrity_return_should_be_of_type_json(client):
 def test_on_verify_integrity_should_return_body_content(client):
     response = client.get('/healthcheck')
     assert response.json() == { "status": "ok" }
+
+def test_on_upload_image_should_return_status_200(client):
+    response = client.post('/colors')
+    assert response.status_code == HTTPStatus.OK
+
+def test_on_upload_image_return_should_be_of_type_json(client):
+    response = client.post('/colors')
+    files = {
+        "file": ("test_image.jpg", open("./tests/utils/sky-7622960_1280.jpg", "rb"), "image/jpg")
+    }
+
+    response = client.post('/colors', files=files, data={
+        'data': json.dumps({"n_colors": 4})
+    }, headers={})
+    assert response.headers['Content-Type'] == 'application/json'
