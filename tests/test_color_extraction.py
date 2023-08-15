88b542e926bfabc138f4b3c730f6c9d1ff8315cc
NatanDuarte <natanprocopio@gmail.com>
2023-08-15 12:38:57 -0300

    tests for color extraction

--- tests/test_color_extraction.py
+++ tests/test_color_extraction.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 import os
 
 from colors_api.color_extractor import ColorExtractor
@@ -10,8 +11,10 @@ def test_color_extractor_should_load_the_input_image():
     colorExtractor.load(test_image_path)
     assert colorExtractor.image is not None
 
-def test_color_extractor_should_only_accept_colorful_images():
-    test_image_path = os.path.join('tests', 'utils', 'sky-7622960_1280-BW.jpg')
+def test_color_extractor_should_should_extract_colors():
+    test_image_path = os.path.join('tests', 'utils', 'sky-7622960_1280.jpg')
     colorExtractor = ColorExtractor()
-    colorExtractor.load(test_image_path)
-    assert colorExtractor.image is not None
+
+    result_json = colorExtractor.load(test_image_path).extract(n_colors=3)
+    result_dict = json.loads(result_json)
+    assert len(result_dict['palette']) == 3
