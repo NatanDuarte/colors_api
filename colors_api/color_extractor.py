b8365d10a8d17de0f93ac164d5782b60a8920bc1
NatanDuarte <natanprocopio@gmail.com>
2023-08-15 12:41:42 -0300

    feat+fix: improving color extraction class, solving color space bug

--- colors_api/color_extractor.py
+++ colors_api/color_extractor.py
@@ -1,19 +1,27 @@
-import json
 import cv2 as cv
 import numpy as np
+
+from collections import Counter
 
 
 class ColorExtractor:
     def __init__(self) -> None:
         self.image = None
-    
+
     def load(self, image_path):
         self.image = cv.imread(image_path)
         return self
-    
+
     def extract(self, n_colors):
         if (self.image.shape == 2):
             self.image = cv.cvtColor(self.image, cv.COLOR_GRAY2BGR)
+        elif self.image.shape[2] == 4:
+            self.image = cv.cvtColor(self.image, cv.COLOR_BGRA2BGR)
+
+        self.image = cv.cvtColor(self.image, cv.COLOR_BGR2RGB)
+
+
+        self._resize_input()
 
         pixels = self.image.reshape(-1, 3)
 
@@ -29,9 +37,26 @@ class ColorExtractor:
             None, criteria, 10, cv.KMEANS_RANDOM_CENTERS
         )
 
-        predominant_colors = []
-        for color in pred_colors:
-            hex_color = f'#{color[0]}{color[1]}{color[2]}'
-            predominant_colors.append(hex_color)
+        color_counts = Counter(labels.flatten())
+        predominant_colors_rgb = \
+            [pred_colors[i] for i, _ in color_counts.most_common(n_colors)]
 
-        return json.dumps({"palette": predominant_colors}, indent=2)
+        predominant_colors_hex = []
+        for color in predominant_colors_rgb:
+            hex_color = self._format_hex(color)
+            predominant_colors_hex.append(hex_color)
+
+        return {"palette": predominant_colors_hex}
+
+    def _resize_input(self):
+        height, width = self.image.shape[:2]
+        new_width = int(width * 0.75)
+        new_height = int(height * 0.75)
+        self.image = cv.resize(
+            self.image, 
+            (new_width, new_height), 
+            interpolation=cv.INTER_LINEAR
+        )
+
+    def _format_hex(self, color):
+        return f'#{int(color[0]):02x}{int(color[1]):02x}{int(color[2]):02x}'
