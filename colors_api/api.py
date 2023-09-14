78ac3997ce827c1c969ab11e24ababda2c0c863d
NatanDuarte <natanprocopio@gmail.com>
2023-09-14 14:16:20 -0300

    refactoring routes

--- colors_api/api.py
+++ colors_api/api.py
@@ -1,34 +1,7 @@
-import shutil
-
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse
-from colors_api.color_extractor import ColorExtractor
-
+from fastapi import FastAPI
+from .routes import colors, health
 
 app = FastAPI()
 
-@app.get('/healthcheck')
-def healthcheck():
-    return { "status": "ok" }
-
-@app.post('/colors')
-def extract_colors(n_colors: int, file: UploadFile = File(...)):
-    try:
-        if file.content_type.startswith('image/'):
-            temp_file = f'./temp/{file.filename}'
-            with open(temp_file, "wb") as buffer:
-                shutil.copyfileobj(file.file, buffer)
-            
-            colorExtractor = ColorExtractor()
-
-            palette_json = colorExtractor.load(temp_file).extract(n_colors)
-
-            return JSONResponse(content=palette_json, status_code=200)
-        else:
-            return JSONResponse(content={
-                "message": "no image provided"
-            }, status_code=400)
-    except Exception as e:
-        return JSONResponse(content={
-            "message": f"Erro ao processar a imagem: {str(e)}"
-        }, status_code=500)
+app.include_router(colors.router)
+app.include_router(health.router)
